f848f7f2ad85703e3b7e3d7182f511bc10c72a7b
Yongzheng Huang <iamhuangyz@gmail.com>
2016-05-23 11:55:55 -0400

    create models

--- locator_cam/locator_cam_app/admin.py
+++ locator_cam/locator_cam_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from locator_cam_app.models import Photo, Moment, UserProfile
 
 # Register your models here.
+admin.site.register(Photo)
+admin.site.register(Moment)
+admin.site.register(UserProfile)
--- locator_cam/locator_cam_app/models.py
+++ locator_cam/locator_cam_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+import hashlib
 
-# Create your models here.
+class Photo(models.Model):
+	base64_image_str = models.TextField()
+
+	def __str__(self):
+		return self.base64_image_str
+
+class Moment(models.Model):
+	description = models.TextField()
+	latitude = models.FloatField()
+	longitude = models.FloatField()
+	user_profile = models.ForeignKey(User)
+	photo = models.OneToOneField(Photo)
+	thumbnail_base64 = models.TextField()
+	pub_time = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.description
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+	friends = models.ManyToManyField('self')
+
+	def __str__(self):
+		return self.user.username
