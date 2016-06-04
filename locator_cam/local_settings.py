62db9b42413ea14b76214ebe2f28adb50b8802e3
Yongzheng Huang <iamhuangyz@gmail.com>
2016-06-04 18:39:38 -0400

    added setting for local postgresql database

---
+++ locator_cam/local_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'locatorcam',
+        'USER': 'locatorcamuser',
+        'PASSWORD': 'locatorcamuser',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
+
+DEBUG = True
